ffeea9650426761a6d25f70affdf2bde0506b0ff
33sakib33 <48786667+33sakib33@users.noreply.github.com>
2020-02-11 00:39:24 +0600

    Add files via upload

--- test3.cpp
+++ test3.cpp
@@ -3,73 +3,96 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-typedef struct bins{
-    int *array;
-    int count;
-}bin;
-bin *b;
-void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]){
+typedef struct binStructure
+{
+    int *arrayOfElements;
+    int countOfElements;
+} bins;
 
-    double *a;
-    a=mxGetPr(prhs[0]);
-    int min=10000000,max=0;
-    int n=mxGetNumberOfElements(prhs[0]);
-    double *k;
-    k=mxGetPr(prhs[1]);
-    int pots=(int)*k;
-    mexPrintf("%d\n",pots);
-    int width=(n)/pots;
-    b = (bin*)calloc(pots,sizeof(bin));
-    for(int i=0;i<pots;i++){
-        b[i].array=(int*)calloc(width,sizeof(int));
+bins *bin;
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+
+    double *inputArray;
+    pointerForTheArray=mxGetPr(prhs[0]);
+    //int min=10000000,max=0;
+    int numberOfElementsInTheInputArray=mxGetNumberOfElements(prhs[0]);
+    double *numberOfBins;
+    numberOfBins=mxGetPr(prhs[1]);
+    int binCount=(int)*numberOfBins;
+    //mexPrintf("%d\n",pots);
+    int widthOfTheBins=(numberOfElementsInTheInputArray)/binCount;
+    bin = (bins*)calloc(binCount,sizeof(bins));
+
+    for(int iterator1=0; i<binCount; iterator1++)
+    {
+
+        bin[iterator1].arrayOfElements=(int*)calloc(width,sizeof(int));
     }
-    int count;
-    int count1=0;
-    int count2=0;
+
+    int countOfElementsInTheBin;
+    int countSignalForNewBin=0;
+    int countForBins=0;
     //printf("%d\n",width);
-    for(int i=0;i<10;i++){
-            //printf("d");
-        for(int j=i*width;j<(i+1)*width;j++){
-            if(10<=j){
+    for(int iterator1=0; iterator1<10; iterator1++)
+    {
+        //printf("d");
+        for(int iterator2=iterator1*width; iterator2<(iiterator1+1)*width; j++)
+        {
+
+            if(numberOfElementsInTheInputArray<=iterator2)
+            {
                 //printf("d");
                 break;
 
             }
-            else if(count2>=pots){
+            else if(countofrBins>=binCount)
+            {
+
                 break;
+
             }
-            else{
+            else
+            {
                 //printf("%d\n",count2);
-                count=b[count2].count;
-                b[count2].array[count]=a[j];
-                count++;
-                b[count2].count=count;
-                count1++;
-                if(count1>=2){
-                    count1=0;
-                    count2++;
+                countOfElementsInTheBin=b[countForBins].countOfElements;
+                bin[countForBins].arrayOfElements[countOfElements]=inputArray[iterator2];
+                countOfElementsInTheBin++;
+                bin[countforBins].countOfElementsInTheBin=countOfElementsInTheBin;
+                countSignalForNewBin++;
+                if(countSignalForNewBin>=2)
+                {
+                    countSignalForNewBin=0;
+                    countforBins++;
                 }
             }
         }
     }
-    for(int i=0;i<5;i++){
-        for(int j=0;j<b[i].count;j++){
-            mexPrintf("%d ",b[i].array[j]);
+    for(int iterator1=0; iterator1<5; iterator1++)
+    {
+        for(int iteartor2=0; iterator2<bin[iterator1].countOfElements; iterator2++)
+        {
+            mexPrintf("%d ",bin[iteartor1].arrayOfElements[iterator2]);
         }
         mexPrintf("\n");
     }
     //double *ptr;
-    
+
     plhs[0]=mxCreateDoubleMatrix(pots,width,mxREAL);
-    double *ptr2;
-    ptr2=mxGetPr(plhs[0]);
-        count=0;
-       for(int i=0;i<pots;i++){
-        for(int j=0;j<b[i].count;j++){
-            ptr2[count]=b[i].array[j];
-            count++;   
+    double *pointerForOutput;
+    pointerForOutput=mxGetPr(plhs[0]);
+    int outputIndex=0;
+    for(int iterator1=0; iterator1<binCount; iterator1++)
+    {
+
+        for(int iterator2=0; iterator2<b[iterator1].countOfElements; iterator2++)
+        {
+
+            pointerForOutput[outputIndex]=bin[iterator1].array[iterator2];
+            outputIndex++;
         }
     }
-    
-    
+
+
 }
